65def9cadfde3f73ebeda3d5599685fdf96e184b
Dallas James <dallasjames42@gmail.com>
2019-12-13 18:38:31 -0700

    MVP reached

--- data/routers/projects.js
+++ data/routers/projects.js
@@ -19,7 +19,27 @@ router.get("/:id", (req, res) => {
 router.get('/:id/actions', (req, res) => {
     project.getProjectActions(req.params.id)
         .then(data => res.status(200).json(data))
-        .catch(err => res.status(500).json({ error: "i screwed up"}));
+        .catch(err => res.status(500).json({error: "i screwed up"}));
 });
+
+router.post("/", (req, res) => {
+    project.insert(req.body)
+        .then(project => res.status(201).json(project))
+        .catch(err => res.status(500).json({error: "i fix later"}))
+})
+
+
+router.delete("/:id", (req, res) => {
+    project.remove(req.params.id)
+    .then(project => res.status(204).json(project))
+    .catch(err => res.status(500).json({error: "it didnt work"}))
+})
+
+router.put("/:id", (req, res) => {
+    let id = req.params.id
+    project.update(id, req.body)
+        .then(project => res.status(200).json(project))
+        .catch(err => res.status(500).json({error: "not finsihed yet"}))
+})
 
 module.exports = router
